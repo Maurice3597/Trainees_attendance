acb256686d7bb10ef377eb5978b4ef963ccc9518
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2025-03-02 03:39:07 +0100

    Added Pattern fill to sheets

--- Sample_2.py
+++ Sample_2.py
@@ -1,7 +1,7 @@
 import calendar
 from datetime import datetime, timedelta
 from openpyxl import Workbook
-from openpyxl.chart import BarChart,LineChart, Reference
+from openpyxl.chart import BarChart, Reference
 from openpyxl.worksheet.datavalidation import DataValidation
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font, Color, PatternFill, Border, Side, Alignment
@@ -18,7 +18,7 @@ def create_month_sheet(workbook, month, year):
     # sheet.freeze_panes = "A15"  # Freeze rows up to row 14
 
     # Set the header row
-    sheet["D15"] = "Name"
+    sheet["D17"] = "Name"
     current_date = datetime(year, list(calendar.month_name).index(month), 1)
     col = 5
     total_days = 0
@@ -26,10 +26,10 @@ def create_month_sheet(workbook, month, year):
     while current_date.month == list(calendar.month_name).index(month):
         if current_date.strftime("%A") in DAYS_OF_WEEK:
             total_days += 1
-            day_cell = sheet.cell(row=15, column=col)
-            date_cell = sheet.cell(row=16, column=col)
+            day_cell = sheet.cell(row=17, column=col)
+            date_cell = sheet.cell(row=18, column=col)
             day_cell.value = current_date.strftime("%a")
-            date_cell.value = current_date.strftime("%d-%b")
+            date_cell.value = int(current_date.strftime("%d"))
             day_cell.alignment = date_cell.alignment = Alignment(horizontal="center")
             col += 1
         current_date += timedelta(days=1)
@@ -37,14 +37,14 @@ def create_month_sheet(workbook, month, year):
     # Add average attendance and remarks columns
     for K in ATTENDANCE_OPTIONS:
         k = col + ATTENDANCE_OPTIONS.index(K) + 1
-        sheet.cell(row=15, column=k).value = K
+        sheet.cell(row=17, column=k).value = K
 
     attendance_col = col + 7
     avg_col = col + 8
     remarks_col = col + 9
-    sheet.cell(row=15, column=attendance_col).value = "Attendance"
-    sheet.cell(row=15, column=avg_col).value = "Average Attendance (%)"
-    sheet.cell(row=15, column=remarks_col).value = "Remarks"
+    sheet.cell(row=17, column=attendance_col).value = "Attendance"
+    sheet.cell(row=17, column=avg_col).value = "Average Attendance (%)"
+    sheet.cell(row=17, column=remarks_col).value = "Remarks"
 
     return sheet, avg_col, remarks_col, total_days
 
@@ -70,12 +70,12 @@ def add_summary(workbook, workers, total_days_dict):
         # Add the data validation to each sheet
         sheet.add_data_validation(dv)
 
-        for row in range(17, 17 + len(workers)):
+        for row in range(19, 19 + len(workers)):
             for column in range(total_days + 6, total_days + 11):
                 position = column - total_days - 6
                 if total_days == 23:
                     opt_list = list(dict_day[23].values())
-                    sheet.cell(row=row, column=column ).value = f"=COUNTIF(E{row}:AA{row},${opt_list[position]}$15)"
+                    sheet.cell(row=row, column=column ).value = f"=COUNTIF(E{row}:AA{row},${opt_list[position]}$17)"
                     sheet[f"AI{row}"].value = f"=SUM(AC{row}:AD{row})"
                     sheet[f"AJ{row}"].value = f"=ROUND(AI{row}/{0.01 * total_days}, 1)"
                     dv.add(f"E{row}:AA{row}")
@@ -84,21 +84,21 @@ def add_summary(workbook, workers, total_days_dict):
 
                 elif total_days == 22:
                     opt_list = list(dict_day[22].values())
-                    sheet.cell(row=row, column=column ).value = f"=COUNTIF(E{row}:Z{row},${opt_list[position]}$15)"
+                    sheet.cell(row=row, column=column ).value = f"=COUNTIF(E{row}:Z{row},${opt_list[position]}$17)"
                     sheet[f"AH{row}"].value = f"=SUM(AB{row}:AC{row})"
                     sheet[f"AI{row}"].value = f"=ROUND(AH{row}/{0.01 * total_days}, 1)"
                     dv.add(f"E{row}:Z{row}")
 
                 elif total_days == 21:
                     opt_list = list(dict_day[21].values())
-                    sheet.cell(row=row, column=column ).value = f"=COUNTIF(E{row}:Y{row},${opt_list[position]}$15)"
+                    sheet.cell(row=row, column=column ).value = f"=COUNTIF(E{row}:Y{row},${opt_list[position]}$17)"
                     sheet[f"AG{row}"].value = f"=SUM(AA{row}:AB{row})"
                     sheet[f"AH{row}"].value = f"=ROUND(AG{row}/{0.01 * total_days}, 1)"
                     dv.add(f"E{row}:Y{row}")
 
                 elif total_days == 20:
                     opt_list = list(dict_day[20].values())
-                    sheet.cell(row=row, column=column ).value = f"=COUNTIF(E{row}:X{row},${opt_list[position]}$15)"
+                    sheet.cell(row=row, column=column ).value = f"=COUNTIF(E{row}:X{row},${opt_list[position]}$17)"
                     sheet[f"AF{row}"].value = f"=SUM(Z{row}:AA{row})"
                     sheet[f"AG{row}"].value = f"=ROUND(AF{row}/{0.01 * total_days}, 1)"
                     dv.add(f"E{row}:X{row}")
@@ -112,11 +112,11 @@ def add_summary(workbook, workers, total_days_dict):
                                 f'IF({avg_cell.coordinate}>50.00,"Average","Poor")))')
 
         # Add a row for total attendance for each day
-        sheet.cell(row=40, column=4).value = "Total Attendance"
+        sheet.cell(row=42, column=4).value = "Total Attendance"
         for col in range(5, total_days + 5):
             col_letter = get_column_letter(col)  # Convert column index to letter
-            sheet.cell(row=40, column=col).value = (f'=COUNTIFS({col_letter}{17}:{col_letter}{len(workers)+17}, "P") +'
-                                                    f'COUNTIFS({col_letter}{17}:{col_letter}{len(workers)+17}, "L")')
+            sheet.cell(row=42, column=col).value = (f'=COUNTIFS({col_letter}{19}:{col_letter}{len(workers)+19}, "P") +'
+                                                    f'COUNTIFS({col_letter}{19}:{col_letter}{len(workers)+19}, "L")')
 
 
 # Function to plot charts
@@ -130,47 +130,132 @@ def plot_charts(workbook, workers, total_days_dict):
         chart_1.title = f"Individual Average Daily Attendance ({sheet.title})"
         chart_1.x_axis.title = "Attendees"
         chart_1.y_axis.title = "Days_present"
-        data_1 = Reference(sheet, min_col=avg_col, min_row=16, max_row=16 + len(workers))
+        data_1 = Reference(sheet, min_col=avg_col, min_row=18, max_row=18 + len(workers))
         chart_1.add_data(data_1, titles_from_data=True)
         sheet.add_chart(chart_1, f"E2")
         sheet.column_dimensions['D'].width = 20 
 
         # Add line chart_1 for collective attendance
         chart_2 = BarChart()
-        chart_2.title = "Total Daily Attendance Over"
+        chart_2.title = "Total Daily Attendance"
         chart_2.x_axis.title = "Dates"
         chart_2.y_axis.title = "Total Attendance"
         # Define the data for the chart_1
         data_2 = Reference(sheet,
-                         min_col=2,  # Starting from column B
-                         min_row=40,
-                         max_col=total_days + 1,  # Up to column X (which is 24)
-                         max_row=40)  # Only the 40th row
+                         min_col=4,  # Starting from column D
+                         min_row=42,
+                         max_col=total_days + 4,
+                         max_row=42)  # Only the 42nd row
 
         # Define the x-axis labels (dates) from row 1 (assuming headers are in row 1)
         dates = Reference(sheet,
-                          min_col=2,
-                          min_row=16,
-                          max_col=total_days + 1,  # Same number as max_col data
-                          max_row=16)  # Assume headers are in row
+                          min_col=4,
+                          min_row=18,
+                          max_col=total_days + 4,  # Same number as max_col data
+                          max_row=18)  # Assume headers are in row
         chart_2.add_data(data_2, from_rows=False)
         chart_2.set_categories(dates)
         sheet.add_chart(chart_2, f"Q2")
 
+def color_fill(colour: str):
+    return PatternFill(start_color=f'{colour}', end_color=f'{colour}', fill_type='solid')
+
 def format_sheet(workbook, workers, total_days_dict):
     for sheet in workbook.worksheets:
         total_days = total_days_dict[sheet.title]
-        # Create a light blue fill pattern
-        light_blue_fill = PatternFill(start_color='ADD8E6', end_color='ADD8E6', fill_type='solid')
+
+        end_col = get_column_letter(total_days+17)
+        # Formating row 1 to row 16 and column A
+        cell_row_range = sheet[f'A4:{end_col}16']
+        for row in cell_row_range:
+            for cell_row in row:
+                cell_row.fill = color_fill('f1f3fd')
+
+        for row in range(17, 101):  # 101 is excluded
+            sheet[f'A{row}'].fill = color_fill('f1f3fd') # For column A
+            sheet[f'B{row}'].fill = color_fill('fdfefe') # For column B
+
+        # General background, text and border formatting
+        cell_range = sheet[f'C17:{end_col}100']
+        font_style = Font(name='Arial', size=9, bold=True, color='000000')  # Black, bold font
+        border_style = Border(left=Side(style='none'),
+                             right=Side(style='none'),
+                             top=Side(style='hair'),
+                            bottom=Side(style='hair'))  # Thin border
+        alignment_style = Alignment(horizontal='center', vertical='center')
+
+        for row in cell_range:
+            for cell in row:
+                cell.font = font_style
+                cell.fill = color_fill("fdfefe")
+                cell.border = border_style
+                cell.alignment = alignment_style
+
+        # formatting for first 3 rows
+        row_1_3 = sheet[f'A1:{end_col}3']
+        font_style_1_3 = Font(name='Arial', size=9, bold=True, color='ffffff')
+        for row in  row_1_3:
+            for cell_1_3 in row:
+                cell_1_3.font = font_style_1_3
+                cell_1_3.fill = color_fill('7aa0f8')
+                cell_1_3.alignment = alignment_style
+
+
+
+        # Create conditional fill pattern for cells with value P
         for column in range(5, total_days + 5):
             col_letter = get_column_letter(column)
-            cell_range = f"{col_letter}17:{col_letter}{17+len(workers)}"
-            #for cell_range in cell_ranges:# Change this range as needed
+            cell_range = f"{col_letter}19:{col_letter}{19+len(workers)}"
             # Add conditional formatting rule
             sheet.conditional_formatting.add(
                 cell_range,
-                CellIsRule(operator='equal', formula=['"P"'], fill=light_blue_fill)
+                CellIsRule(operator='equal', formula=['"P"'], fill=color_fill('ADD8E6'))
             )
+
+        # Add conditional formating to the remarks column
+        ave_col_let = get_column_letter(total_days + 14)
+        remarks_color= {'28a745': "Excellent", '86c3e0': "Good", 'ffc107': "Average", 'dc3545': "Poor"}
+        cell_range = f"{ave_col_let}19:{ave_col_let}{19 + len(workers)}"
+        for colour, remark in remarks_color.items():
+            sheet.conditional_formatting.add(
+                cell_range,
+                CellIsRule(operator='equal', formula=[f'"{remark}"'], fill=color_fill(colour)))
+
+        # Format the day and date rows
+        omit = list([get_column_letter(total_days + x) for x in range(5, 12, 6)])
+        omit_cells = {f'{col}17' for col in omit}.union({f'{col}18' for col in omit}) # Create a set of omitted cell
+        for row in sheet[f'C17:{ave_col_let}18']:
+            for cell in row:
+                if cell.coordinate not in omit_cells:
+                    cell.fill = color_fill('7ab0fa')
+                    cell.font = font_style_1_3
+
+        # color formatting for columns where the date is Monday,
+        wed_thu = ['Wed', 'Thu'] # Wednesday or Thursday
+        start_col, end_col = 3, 3 + total_days
+        for row in range(19, 101):
+            for col in range(start_col, end_col + 1):
+                column_letter = get_column_letter(col)
+                if sheet[f'{column_letter}17'].value == "Mon":
+                    cells = sheet.cell(row=row, column=col)
+                    cells.fill = color_fill('DEE4F2') #
+                elif sheet[f'{column_letter}17'].value in wed_thu:
+                    cells = sheet.cell(row=row, column=col)
+                    cells.fill = color_fill('ededee')
+
+        # Pattern fill for the remaining (Unused cells)
+        for row in range(1, 101):
+            for cols in range(total_days + 18, 351):
+                cell_un = sheet.cell(row=row, column=cols)
+                cell_un.fill = color_fill('CCE8CF')
+
+        for row in range(101, 351):
+            # column_let = get_column_letter(col)
+            for col in range(1, 531):
+                cell_un = sheet.cell(row=row, column=col)
+                cell_un.fill = color_fill('CCE8CF')
+
+def apply_
 
 
 
@@ -193,7 +278,7 @@ def main():
         total_days_dict[month] = total_days
 
         # Add worker names
-        for i, worker in enumerate(workers, start=17):
+        for i, worker in enumerate(workers, start=19):
             sheet.cell(row=i, column=4).value = worker
 
     # Add attendance summary
